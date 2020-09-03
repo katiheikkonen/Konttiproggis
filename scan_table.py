2c9edcf2d06fbdbb60b5be6e1dc9cab2f62684c6
katiheikkonen <kati.m.heikkonen@gmail.com>
2020-09-03 13:04:39 +0300

    added cloudformation ecs

--- scan_table.py
+++ scan_table.py
@@ -2,6 +2,7 @@ import boto3
 
 dynamo_client = boto3.client('dynamodb', region_name='eu-west-1')
 
+#  function fetches all the information from the DynamoDB-table named Student
 def get_items():
     return dynamo_client.scan(
         TableName='Student'
